db60046fb0cc0368f304c5c6886f80702c87ef26
Commercial <135322806+alinavasileva-cco@users.noreply.github.com>
2026-07-16 23:08:14 +0300

    Connect approved mobile hero styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 import "./responsive-fixes.css";
 import "./typography-fixes.css";
+import "./mobile-hero.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
