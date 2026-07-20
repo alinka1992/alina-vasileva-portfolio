9ed4435f85764e92f9f9df9cdb77630cbea61619
Commercial <135322806+alinavasileva-cco@users.noreply.github.com>
2026-07-20 17:37:16 +0300

    Activate final verified responsive build

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./final-site.css";
-import "./final-polish.css";
-import "./funnel-final.css";
+import "./final-v2.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
