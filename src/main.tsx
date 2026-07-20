ac7d2c1d833970edb371ea59eee1615cf921b363
Commercial <135322806+alinavasileva-cco@users.noreply.github.com>
2026-07-20 16:14:45 +0300

    Enable final ecommerce funnel visualization

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./final-site.css";
 import "./final-polish.css";
+import "./funnel-final.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
